27f6871ccc361f582b3d98802c87b2d34f91390d
Andrija Petrovic <andrija.hers@gmail.com>
2016-07-15 15:57:30 +0200

    Asynchronous production

--- producercreator.js
+++ producercreator.js
@@ -2,6 +2,7 @@ function createProduceLink (execlib, applinkinglib) {
   'use strict';
 
   var lib = execlib.lib,
+    q = lib.q,
     FilterHandler = require('./filterhandlercreator')(execlib);
 
   // checkers
@@ -43,18 +44,23 @@ function createProduceLink (execlib, applinkinglib) {
       s = instanceFromString(eb, sa[0]);
       t = instanceFromString(eb, ta[0]);
       if (s && t) {
-        return {
-          s: s,
-          sr: sa[1],
-          t: t,
-          tr: ta[1]
-        };
+        return q.all([q(s), q(t)]).spread(function (s, t) {
+          var ret = {
+            s: s,
+            sr: sa[1],
+            t: t,
+            tr: ta[1]
+          };
+          sa = null;
+          ta = null;
+          return ret;
+        });
       } else {
         if (!s) {
           console.error('source could not be found from', sa[0]);
         }
         if (!t) {
-          console.error('target could not be found from', ta[0], 'on', eb);
+          console.error('target could not be found from', ta[0]);
         }
       }
     }
@@ -73,50 +79,63 @@ function createProduceLink (execlib, applinkinglib) {
   };
 
   // producers
+  function doProduceEvent2PropertyLink (eb, desc, pes) {
+    var fh, ehctor, eh;
+    console.log('pes', pes);
+    ehctor = applinkinglib.eventEmitterHandlingRegistry.resolve({emitter:pes.s, name:pes.sr});
+    if (ehctor) {
+      eh = new ehctor(pes.s, pes.sr);
+      fh = new FilterHandler(desc.filter, pes.t.set.bind(pes.t, pes.tr));
+      addLink(eb, desc.name, new LinkingResult([eh.listenToEvent(fh.processInput.bind(fh)), eh, fh]), pes);
+    }
+  }
   function produceEvent2PropertyLink (eb, desc) {
-    var pes = parsedEventString(eb, desc, '!', ':'), fh, ehctor, eh;
+    var pes = parsedEventString(eb, desc, '!', ':');
     if (pes) {
-      ehctor = applinkinglib.eventEmitterHandlingRegistry.resolve({emitter:pes.s, name:pes.sr});
-      if (ehctor) {
-        eh = new ehctor(pes.s, pes.sr);
-        fh = new FilterHandler(desc.filter, pes.t.set.bind(pes.t, pes.tr));
-        addLink(eb, desc.name, new LinkingResult([eh.listenToEvent(fh.processInput.bind(fh)), eh, fh]), pes);
-      }
+      pes.then(doProduceEvent2PropertyLink.bind(null, eb, desc));
+    }
+  }
+  function doProduceEvent2FunctionLink (eb, desc, pes) {
+    var fh, ehctor, eh, func;
+    func = pes.t[pes.tr];
+    if (!lib.isFunction(func)) {
+      console.error(pes.tr, 'is not a method of', pes.t);
+      return;
+    }
+    ehctor = applinkinglib.eventEmitterHandlingRegistry.resolve({emitter:pes.s, name:pes.sr});
+    if (ehctor) {
+      eh = new ehctor(pes.s, pes.sr);
+      fh = new FilterHandler(desc.filter, func.bind(pes.t));
+      addLink(eb, desc.name, new LinkingResult([eh.listenToEvent(fh.processInput.bind(fh)), eh, fh]), pes);
     }
   }
   function produceEvent2FunctionLink (eb, desc) {
-    var pes = parsedEventString(eb, desc, '!', '>'), fh, ehctor, eh, func;
+    var pes = parsedEventString(eb, desc, '!', '>');
     if (pes) {
-      func = pes.t[pes.tr];
-      if (!lib.isFunction(func)) {
-        console.error(pes.tr, 'is not a method of', pes.t);
-        return;
-      }
-      ehctor = applinkinglib.eventEmitterHandlingRegistry.resolve({emitter:pes.s, name:pes.sr});
-      if (ehctor) {
-        eh = new ehctor(pes.s, pes.sr);
-        fh = new FilterHandler(desc.filter, func.bind(pes.t));
-        addLink(eb, desc.name, new LinkingResult([eh.listenToEvent(fh.processInput.bind(fh)), eh, fh]), pes);
-      }
+      pes.then(doProduceEvent2FunctionLink.bind(null, eb, desc));
+    }
+  }
+  function doProduceProperty2PropertyLink (eb, desc, pes) {
+    var fh, phctor, ph, pc;
+    phctor = applinkinglib.propertyTargetHandlingRegistry.resolve({carrier: pes.t, name: pes.tr});
+    if (!phctor) {
+      return;
+    }
+    ph = new phctor(pes.t, pes.tr);
+    fh = new FilterHandler(desc.filter, ph.handle.bind(ph));
+    pc = pes.s.attachListener.length;
+    if (pc === 2) {
+      addLink(eb, desc.name, new LinkingResult([pes.s.attachListener(pes.sr, fh.processInput.bind(fh)), fh, ph]), pes);
+      return;
+    }
+    if (pc ===3) {
+      addLink(eb, desc.name, new LinkingResult([pes.s.attachListener('changed', pes.sr, fh.processInput.bind(fh)), fh, ph]), pes);
     }
   }
   function produceProperty2PropertyLink (eb, desc) {
-    var pes = parsedEventString(eb, desc, ':', ':'), fh, phctor, ph, pc;
+    var pes = parsedEventString(eb, desc, ':', ':');
     if (pes) {
-      phctor = applinkinglib.propertyTargetHandlingRegistry.resolve({carrier: pes.t, name: pes.tr});
-      if (!phctor) {
-        return;
-      }
-      ph = new phctor(pes.t, pes.tr);
-      fh = new FilterHandler(desc.filter, ph.handle.bind(ph));
-      pc = pes.s.attachListener.length;
-      if (pc === 2) {
-        addLink(eb, desc.name, new LinkingResult([pes.s.attachListener(pes.sr, fh.processInput.bind(fh)), fh, ph]), pes);
-        return;
-      }
-      if (pc ===3) {
-        addLink(eb, desc.name, new LinkingResult([pes.s.attachListener('changed', pes.sr, fh.processInput.bind(fh)), fh, ph]), pes);
-      }
+      pes.then(doProduceProperty2PropertyLink.bind(null, eb, desc));
     }
   }
   // producers end
