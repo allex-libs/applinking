585edbe4a4267bc4ac98d2878d600e99a3b64708
Veljko Popovic <v.s.popovic@gmail.com>
2016-07-18 09:51:50 +0200

    moving along, rebuilding web_component

--- web_component/dist/browserified.js
+++ web_component/dist/browserified.js
@@ -159,23 +159,18 @@ function createProduceLink (execlib, applinkinglib) {
   'use strict';
 
   var lib = execlib.lib,
+    q = lib.q,
     FilterHandler = require('./filterhandlercreator')(execlib);
 
   // checkers
-  function isEventSource(desc) {
-    return desc && desc.source && desc.source.indexOf('!') > 0;
+  function isEvent(subdesc) {
+    return lib.isString(subdesc) && subdesc.indexOf('!') > 0;
   }
-  function isPropertySource(desc) {
-    return desc && desc.source && desc.source.indexOf(':') > 0;
+  function isProperty(subdesc) {
+    return lib.isString(subdesc) && subdesc.indexOf(':') > 0;
   }
-  function isEventTarget(desc) {
-    return desc && desc.target && desc.target.indexOf('!') > 0;
-  }
-  function isPropertyTarget(desc) {
-    return desc && desc.target && desc.target.indexOf(':') > 0;
-  }
-  function isFunctionTarget(desc) {
-    return desc && desc.target && desc.target.indexOf('>') > 0;
+  function isFunction(subdesc) {
+    return lib.isString(subdesc) && subdesc.indexOf('>') > 0;
   }
   // checkers end
 
@@ -183,30 +178,23 @@ function createProduceLink (execlib, applinkinglib) {
   function instanceFromString (eb, str) {
     return str === '.' ? eb : eb.getElement(str);
   }
-  function parsedEventString(eb, desc, sourcedelim, targetdelim) {
-    var sa = desc.source.split(sourcedelim),
-      ta = desc.target.split(targetdelim),
-      s, t;
-    if (sa.length === 2 && ta.length===2) {
-      s = instanceFromString(eb, sa[0]);
-      t = instanceFromString(eb, ta[0]);
-      if (s && t) {
-        return {
-          s: s,
-          sr: sa[1],
-          t: t,
-          tr: ta[1]
-        };
-      } else {
-        if (!s) {
-          console.error('source could not be found from', sa[0]);
-        }
-        if (!t) {
-          console.error('target could not be found from', ta[0], 'on', eb);
-        }
+  function parseEventElementString(eb, desc, delim) {
+    var ea = desc.split(delim), i;
+    if (ea.length === 2) {
+      i = instanceFromString(eb, ea[0]);
+      if (i) {
+        return q(i).then(function (i) {
+          var ret = {
+            instance: i,
+            reference: ea[1]
+          };
+          ea = null;
+          return ret;
+        });
       }
+    } else {
+      return q.reject(new lib.Error('INVALID_LINK_ELEMENT_STRING', 'Invalid link element string: '+desc));
     }
-    return null;
   }
   //parsers end
 
@@ -221,51 +209,76 @@ function createProduceLink (execlib, applinkinglib) {
   };
 
   // producers
-  function produceEvent2PropertyLink (eb, desc) {
-    var pes = parsedEventString(eb, desc, '!', ':'), fh, ehctor, eh;
-    if (pes) {
-      ehctor = applinkinglib.eventEmitterHandlingRegistry.resolve({emitter:pes.s, name:pes.sr});
-      if (ehctor) {
-        eh = new ehctor(pes.s, pes.sr);
-        fh = new FilterHandler(desc.filter, pes.t.set.bind(pes.t, pes.tr));
-        addLink(eb, desc.name, new LinkingResult([eh.listenToEvent(fh.processInput.bind(fh)), eh, fh]), pes);
-      }
+  function produceEventSource(pe) {
+    var ehctor, eh;
+    ehctor = applinkinglib.eventEmitterHandlingRegistry.resolve({emitter:pe.instance, name:pe.reference});
+    if (ehctor) {
+      eh = new ehctor(pe.instance, pe.reference);
+      return q([eh.listenToEvent.bind(eh), eh]);
+    } else {
+      return q.reject(new lib.Error('EVENT_EMITTER_NOT_RECOGNIZED', 'EventEmitter not recognized by eventEmitterHandlingRegistry'));
     }
   }
-  function produceEvent2FunctionLink (eb, desc) {
-    var pes = parsedEventString(eb, desc, '!', '>'), fh, ehctor, eh, func;
-    if (pes) {
-      func = pes.t[pes.tr];
-      if (!lib.isFunction(func)) {
-        console.error(pes.tr, 'is not a method of', pes.t);
-        return;
-      }
-      ehctor = applinkinglib.eventEmitterHandlingRegistry.resolve({emitter:pes.s, name:pes.sr});
-      if (ehctor) {
-        eh = new ehctor(pes.s, pes.sr);
-        fh = new FilterHandler(desc.filter, func.bind(pes.t));
-        addLink(eb, desc.name, new LinkingResult([eh.listenToEvent(fh.processInput.bind(fh)), eh, fh]), pes);
-      }
+
+  function producePropertySource(pe) {
+    var pc;
+    if (!(pe && pe.instance && pe.instance.attachListener)) {
+      return q.reject(new lib.Error('NOT_A_PROPERTY_SOURCE', 'Found an instance that has not got a method `attachListener`'));
+    }
+    pc = pe.instance.attachListener.length;
+    if (pc === 2) {
+      var ret = pe.instance.attachListener.bind(pe.instance, pe.reference);
+      return q([ret]);
+    }
+    if (pc ===3) {
+      return q([pe.instance.attachListener.bind(pe.instance, 'changed', pe.reference), pe]);
     }
   }
-  function produceProperty2PropertyLink (eb, desc) {
-    var pes = parsedEventString(eb, desc, ':', ':'), fh, phctor, ph, pc;
-    if (pes) {
-      phctor = applinkinglib.propertyTargetHandlingRegistry.resolve({carrier: pes.t, name: pes.tr});
-      if (!phctor) {
-        return;
-      }
-      ph = new phctor(pes.t, pes.tr);
-      fh = new FilterHandler(desc.filter, ph.handle.bind(ph));
-      pc = pes.s.attachListener.length;
-      if (pc === 2) {
-        addLink(eb, desc.name, new LinkingResult([pes.s.attachListener(pes.sr, fh.processInput.bind(fh)), fh, ph]), pes);
-        return;
-      }
-      if (pc ===3) {
-        addLink(eb, desc.name, new LinkingResult([pes.s.attachListener('changed', pes.sr, fh.processInput.bind(fh)), fh, ph]), pes);
-      }
+  function produceSource (eb, sourcedesc) {
+    if (!sourcedesc) {
+      return q.reject(new lib.Error('INVALID_SOURCE_DESCRIPTOR', sourcedesc+' is an invalid particular source descriptor'));
     }
+    sourcedesc = sourcedesc.trim();
+    if (isEvent(sourcedesc)) {
+      return parseEventElementString(eb, sourcedesc, '!').then(
+        produceEventSource
+      );
+    }
+    if (isProperty(sourcedesc)) {
+      return parseEventElementString(eb, sourcedesc, ':').then(
+        producePropertySource
+      );
+    }
+    if (isFunction(sourcedesc)) {
+      return parseEventElementString(eb, sourcedesc, '>').then(
+        produceFunctionSource
+      );
+    }
+    return q.reject(new lib.Error('UNRECOGNIZED_LINK_SOURCE', 'Not a recognized source descriptor: '+sourcedesc));
+  }
+
+  function combineSources(sources) {
+    var lm, la, retobj, ret;
+    if (sources.length ===1) {
+      return sources[0];
+    }
+    lm = new lib.ListenableMap();
+    la = sources.map(function (s, index) {
+      var ret = index+'';
+      s[0](lm.replace.bind(lm, ret));
+      return ret;
+    });
+    retobj = Object.create({
+      destroy: function () {if (lm) {lm.destroy();} lm = null; la = null},
+      attach: function (cb) {lm.spread(la, cb, true);}
+    });
+    return [retobj.attach, retobj];
+  }
+
+  function produceSourceComposite(eb, sourcedesc) {
+    var ret = q.all(sourcedesc.split(',').map(produceSource.bind(null, eb))).then(combineSources);
+    eb = null;
+    return ret;
   }
   // producers end
 
@@ -274,6 +287,58 @@ function createProduceLink (execlib, applinkinglib) {
     be.addAppLink(name, l, pes);
   }
   // adders end
+
+
+  function onEventTarget(eb, name, filter, source, pe) {
+    return pe;
+  }
+  function onPropertyTarget(eb, name, filter, source, pe) {
+    var fh, phctor, ph;
+    phctor = applinkinglib.propertyTargetHandlingRegistry.resolve({carrier: pe.instance, name: pe.reference});
+    if (phctor) {
+      ph = new phctor(pe.instance, pe.reference);
+      fh = new FilterHandler(filter, ph.handle.bind(ph));
+      source[0] = source[0](fh.processInput.bind(fh));
+      source.push(ph);
+      source.push(fh);
+      addLink(eb, name, new LinkingResult(source));
+    }
+    return pe;
+  }
+  function onFunctionTarget(eb, name, filter, source, pe) {
+    if (!(pe && pe.instance && pe.reference)) {
+      console.log('invalid function target pack', pe);
+      return;
+    }
+    var func = pe.instance[pe.reference], fh;
+    if (!lib.isFunction(func)) {
+      console.error(pe.reference, 'is not a method of', pe.instance);
+      return;
+    }
+    fh = new FilterHandler(filter, func.bind(pe.instance));
+    source[0] = source[0](fh.processInput.bind(fh));
+    source.push(fh);
+    addLink(eb, name, new LinkingResult(source));
+    return pe;
+  }
+  function produceTarget(eb, name, filter, targetdesc, source) {
+    if (isEvent(targetdesc)) {
+      return parseEventElementString(eb, targetdesc, '!').then(
+        onEventTarget.bind(null, eb, name, filter, source)
+      );
+    }
+    if (isProperty(targetdesc)) {
+      return parseEventElementString(eb, targetdesc, ':').then(
+        onPropertyTarget.bind(null, eb, name, filter, source)
+      );
+    }
+    if (isFunction(targetdesc)) {
+      return parseEventElementString(eb, targetdesc, '>').then(
+        onFunctionTarget.bind(null, eb, name, filter, source)
+      );
+    }
+    return q.reject(new lib.Error('TARGET_DESCRIPTOR_NOT_RECOGNIZED', targetdesc+' could not be recognized as a target descriptor'));
+  }
 
   function produceLink (eb, desc) {
     if (!desc) {
@@ -285,32 +350,10 @@ function createProduceLink (execlib, applinkinglib) {
     if (!desc.target) {
       throw new lib.JSONizingError('NO_TARGET_IN_LINK_DESCRIPTOR', desc, 'No target in');
     }
-    if (isEventSource(desc)) {
-      if (isPropertyTarget(desc)) {
-        produceEvent2PropertyLink(eb, desc);
-        return;
-      }
-      if (isEventTarget(desc)) {
-        produceEvent2EventLink(eb, desc);
-        return;
-      }
-      if (isFunctionTarget(desc)) {
-        produceEvent2FunctionLink(eb, desc);
-      }
-    }
-    if (isPropertySource(desc)) {
-      if (isPropertyTarget(desc)) {
-        produceProperty2PropertyLink(eb, desc);
-        return;
-      }
-      if (isEventTarget(desc)) {
-        produceProperty2EventLink(eb, desc);
-        return;
-      }
-      if (isFunctionTarget(desc)) {
-        produceProperty2FunctionLink(eb, desc);
-      }
-    }
+    produceSourceComposite(eb, desc.source.trim()).then(
+      produceTarget.bind(null, eb, desc.name, desc.filter, desc.target.trim())
+    );
+    eb = null;
   }
 
   function produceLinks (eb, links) {
